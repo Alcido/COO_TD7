08f8a3bea46d5794d57fc77a4309751e10955e57
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-04-22 13:12:49 +0200

    [Modify] rajout d'une classe television (exemple d'objet elec)

---
+++ Eclipse Domotique/src/Television.java
@@ -0,0 +1,13 @@
+//classe de test pour montrer un possible fonctionnement
+
+public class Television implements ElectronicObject{
+
+    public void allumer(){
+        System.out.println("Allumer");
+    }
+
+    public void eteindre(){
+        System.out.println("Eteindre");
+    }
+
+}
